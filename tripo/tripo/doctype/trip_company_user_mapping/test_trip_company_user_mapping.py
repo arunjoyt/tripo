989618d98a5d09453725e70e2165d5c145da8bc0
arunjoyt <16219238+arunjoyt@users.noreply.github.com>
2025-06-12 17:55:49 +0530

    added Trip Company User Mapping

---
+++ tripo/tripo/doctype/trip_company_user_mapping/test_trip_company_user_mapping.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, 22Logic and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTripCompanyUserMapping(FrappeTestCase):
+	pass
