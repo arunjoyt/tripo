f4fb5d5fc6534e876914390fec01f5d94b1e19f1
arunjoyt <16219238+arunjoyt@users.noreply.github.com>
2025-06-13 15:21:04 +0530

    added server side code to automatically fill in Trip Company of logged in user, Trip Company is left non-mandotory so that server side validation will kick in

--- tripo/tripo/doctype/trip_customer/trip_customer.py
+++ tripo/tripo/doctype/trip_customer/trip_customer.py
@@ -1,9 +1,20 @@
 # Copyright (c) 2025, 22Logic and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+from frappe import _
 
 class TripCustomer(Document):
-	pass
+	def validate(self):
+		if not self.trip_company:
+			trip_company = frappe.get_value(
+				"Trip Company User Mapping",
+				{"user": frappe.session.user},
+				"trip_company"
+			)
+
+			if not trip_company:
+				frappe.throw(_("No Trip Company mapping found for the logged-in user ({0})").format(frappe.session.user))
+
+			self.trip_company = trip_company
