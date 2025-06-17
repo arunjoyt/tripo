7fd8eb6760c1be546143976e652e848a8034ddf5
arunjoyt <16219238+arunjoyt@users.noreply.github.com>
2025-06-17 17:49:43 +0530

    changes phone number to data filed, phone number field will not allow saving without counttry code. addded default and country cod addition and simple validation on server side

--- tripo/tripo/doctype/trip_customer/trip_customer.py
+++ tripo/tripo/doctype/trip_customer/trip_customer.py
@@ -4,6 +4,7 @@
 import frappe
 from frappe.model.document import Document
 from frappe import _
+import re
 
 class TripCustomer(Document):
 	def validate(self):
@@ -18,3 +19,22 @@ class TripCustomer(Document):
 				frappe.throw(_("No Trip Company mapping found for the logged-in user ({0})").format(frappe.session.user))
 
 			self.trip_company = trip_company
+
+		# validate customer_phone
+		phone = self.customer_phone
+		if phone:
+			phone = phone.strip()
+
+            # Add +91 if no country code is present
+			if not phone.startswith('+'):
+				phone = f'+91 {phone}'
+
+            # Normalize dash to space
+			phone = re.sub(r'-', ' ', phone)
+
+            # Validate the phone format (international)
+			if not re.match(r'^\+\d{1,4}\s?\d{6,15}$', phone):
+				frappe.throw(_("Customer Phone must be a valid number, e.g., +91 9876543210"))
+
+            # Save back the normalized phone
+			self.customer_phone = phone
